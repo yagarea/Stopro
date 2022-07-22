1c6e19940c4c2a9b28d2927240f6aa7321561917
Jan Černý <jc@ucw.cz>
2022-07-22 13:41:50 +0200

    implement silent mode and config command

--- commands.py
+++ commands.py
@@ -1,17 +1,26 @@
 from utils import *
+from os import environ
+from subprocess import call
 
-def cmd_start():
+def cmd_start(arguments, config):
     backup_hosts()
     forbid_sites(config["forbidden_sites"])
-    print("Your self control session started")
+    if not arguments.silent_mode:
+        print("Your self control session started")
 
 
-def cmd_stop():
+def cmd_stop(arguments, config):
     apply_backup()
-    print("Your self control session ended")
+    if not arguments.silent_mode:
+        print("Your self control session ended")
 
 
-def cmd_status():
+def cmd_status(arguments, config):
     print("STATUS")
+
+def cmd_config(arguments, config):
+    editor = environ.get("EDITOR", "/usr/bin/vim")
+    call([editor, config.config_path])
+
 
 
