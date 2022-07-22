1a94b665b880ceb735cf377f1932f605ad4b27b3
Jan Černý <jc@ucw.cz>
2022-07-22 13:41:21 +0200

    add options

--- args.py
+++ args.py
@@ -6,7 +6,7 @@ import argparse
 def get_args():
     arg_parser = argparse.ArgumentParser("StoPro",
             description="Simple utility for productivity and will training.",
-            epilog="hope it helps")
+            epilog="If you find any bug or have feature suggestion you can open issue in project repository: github.com/yagarea/Stopro\n\nPublished under GPLv3 license")
 
     command_parser = arg_parser.add_subparsers(title="commands", dest="command", required=True)
 
@@ -19,6 +19,24 @@ def get_args():
     parser_status = command_parser.add_parser(
             "status",
             help="print info about current session")
+    parser_config = command_parser.add_parser(
+            "config",
+            help="opens config file in editor")
+
+    arg_parser.add_argument(
+            "-s", "--silent",
+            dest="silent_mode",
+            required=False,
+            action="store_true",
+            help="silent mode")
+
+    arg_parser.add_argument(
+            "-c", "--config",
+            dest="config_path",
+            default="/etc/stopro/conf.yml",
+            required=False,
+
+            help="sets custom config file path")
 
     return arg_parser.parse_args()
 
