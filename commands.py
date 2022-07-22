9d109260ce3426bc977b6ff3fe9278650feb3099
Jan Černý <jc@ucw.cz>
2022-07-22 20:40:52 +0200

    use log activity utility

--- commands.py
+++ commands.py
@@ -5,21 +5,27 @@ from subprocess import call
 def cmd_start(arguments, config):
     backup_hosts()
     forbid_sites(config["forbidden_sites"])
+    log_activity()
     if not arguments.silent_mode:
         print("Your self control session started")
 
 
 def cmd_stop(arguments, config):
     apply_backup()
+    log_activity()
     if not arguments.silent_mode:
         print("Your self control session ended")
 
 
 def cmd_status(arguments, config):
-    print("STATUS")
+    state = get_state()
+    if state["running"]:
+        print("running")
+    else:
+        print("not running")
 
 def cmd_config(arguments, config):
-    editor = environ.get("EDITOR", "/usr/bin/vim")
+    editor = environ.get("$EDITOR", "/usr/bin/vim")
     call([editor, arguments.config_path])
 
 
